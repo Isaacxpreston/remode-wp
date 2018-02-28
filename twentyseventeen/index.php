dd979ced600f1d6363e0b9b652cda449803bbebb
Isaac <isaacxpreston@gmail.com>
2018-02-28 15:56:46 -0500

    move forms to container

--- twentyseventeen/index.php
+++ twentyseventeen/index.php
@@ -1,6 +1,14 @@
 <?php get_header(); ?>
 
-<div>
+<h1>Here</h1>
+
+<div id="container">
+
+</div>
+
+<h1>Not Here</h1>
+
+<div id="moveThis">
 	<?php
 		$my_id = 2;
 		$post_id_5369 = get_post($my_id);
@@ -10,5 +18,11 @@
 		echo $content;
 	?>
 </div>
+
+<script>
+	window.onload = function() {
+		document.getElementById('container').appendChild(  document.getElementById('moveThis') )
+	}
+</script>
 
 <?php get_footer();
