ee5d557124eaf5c5ce29b10da2112fc390ad8b6d
Isaac <isaacxpreston@gmail.com>
2018-02-28 20:55:07 -0500

    additional components

--- twentyseventeen/index.php
+++ twentyseventeen/index.php
@@ -1,14 +1,10 @@
 <?php get_header(); ?>
 
-<h1>Here</h1>
+<!-- vue files -->
+<div id=app></div><script type=text/javascript src=wp-content/themes/twentyseventeen/assets/js/manifest.298d74c79f5df4343b14.js></script><script type=text/javascript src=wp-content/themes/twentyseventeen/assets/js/vendor.17d770379dd0b1b3199f.js></script><script type=text/javascript src=wp-content/themes/twentyseventeen/assets/js/app.56435fc63f2c7801688c.js></script>
 
-<div id="container">
 
-</div>
-
-<h1>Not Here</h1>
-
-<div id="moveThis">
+<div id="nf-content-one">
 	<?php
 		$my_id = 2;
 		$post_id_5369 = get_post($my_id);
@@ -19,10 +15,15 @@
 	?>
 </div>
 
-<script>
-	window.onload = function() {
-		document.getElementById('container').appendChild(  document.getElementById('moveThis') )
-	}
-</script>
+	<!-- main styles -->
+	<link href=wp-content/themes/twentyseventeen/assets/css/app.ad45916ef49f7447fb65bde54e1173dd.css rel=stylesheet>
 
-<?php get_footer();
+	<!-- form overwrite styles -->
+	<link rel="stylesheet" href="wp-content/themes/twentyseventeen/assets/css/custom.css" type="text/css" media="all">
+	<style>
+		body, .site-content {
+			background: white !important;
+		}
+		</style>
+
+	<?php get_footer();
